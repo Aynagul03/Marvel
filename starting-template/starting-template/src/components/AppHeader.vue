d4c810db99bc6bcb05b8fb5318e534633b1dfb15
Aynagul Agataeva <aynagulagataeva@MacBook-Air-Aynagul.local>
2022-03-10 01:42:05 +0400

    added marvel

---
+++ starting-template/starting-template/src/components/AppHeader.vue
@@ -0,0 +1,27 @@
+<template>
+    <nav class="navbar navbar-dark bg-dark">
+      <div class="container-fluid">
+        <a class="navbar-brand">{{title}}</a>
+        <form class="d-flex">
+          <input class="form-control me-2" type="search" placeholder="поиск..." aria-label="Search">
+          <button class="btn btn-outline-light" type="submit">Все</button>
+        </form>
+      </div>
+    </nav>
+
+</template>
+
+<script>
+    export default {
+        name: "AppHeader",
+        props: ['title'],
+        data() {
+            return {
+            }
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ starting-template/starting-template/src/components/Spinner.vue
@@ -0,0 +1,15 @@
+<template>
+            <div class="spinner-border text-danger" role="status">
+            <span class="visually-hidden">Loading...</span>
+            </div>
+</template>
+
+<script>
+    export default {
+        name: "Spinner"
+    }
+</script>
+
+<style scoped>
+
+</style>
